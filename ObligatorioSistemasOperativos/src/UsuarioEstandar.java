53947cdc438065895e18b8a71b9ca9cd7a225b62
Santiago Rius <sjriusperez@gmail.com>
2021-03-25 19:46:40 -0300

    Clases creadas

--- ObligatorioSistemasOperativos/src/UsuarioEstandar.java
+++ ObligatorioSistemasOperativos/src/UsuarioEstandar.java
@@ -8,13 +8,6 @@
  *
  * @author Santiago
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
+public class UsuarioEstandar extends Usuario{
     
 }
