dd780d80b66d8eee5a7c7a5bd91239cc715360d7
belugabi <belugabi@icloud.com>
2021-04-02 18:04:02 -0300

    Atributos de archivo.

--- ObligatorioSistemasOperativos/src/FileSystem/Archivo.java
+++ ObligatorioSistemasOperativos/src/FileSystem/Archivo.java
@@ -11,5 +11,29 @@ package FileSystem;
  * @author Santiago
  */
 public class Archivo {
+    private String nombre;
+    private String contenido;
+    
+    public Archivo(String nombre) {
+        this.nombre = nombre;
+        this.contenido = "";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    
+    public void agregarContenido (String texto){
+        String nuevoTexto = this.contenido+texto;
+        this.contenido = nuevoTexto;
+    }
+    
+    public String mostrarContenido(){
+        return this.contenido;
+    }
     
 }
